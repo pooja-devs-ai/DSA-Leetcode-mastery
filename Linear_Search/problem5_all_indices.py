f7c406126418baffce5c3f6650e9d56e70212b85
pooja-devs-ai <poojamm1805@gmail.com>
2025-07-04 13:15:32 +0530

    Restore deleted Linear Search files from previous commit

---
+++ Linear_Search/problem5_all_indices.py
@@ -0,0 +1,24 @@
+''' Problem Statement:
+
+-->Given an array of integers and a target value,
+-->return a list of all indices where the target appears in the array.
+-->If the target does not exist in the array, return an empty list.'''
+
+#making use of an empty list to store all the indices of the target from the array.
+
+def find_all_indices(arr,target):
+    all_indices=[]
+    for i,value in enumerate(arr):          #range can also be used in place of enumerate()
+        if value==target:
+            all_indices.append(i)
+
+    return all_indices
+
+array=[34,5,21,78,5,65,8,9,5,2]
+target=5
+result=find_all_indices(array,target)
+
+if target in array:
+    print(f" The target element {target} is present at {result} indices.")
+else:
+    print(f" The target element is not found in the array so the list all_indices is {result}.")
